d4552f0d5d7be20be8010115fa318698590e7490
DEEPAK.TYAGI <tyagideepak607@gmail.com>
2025-04-20 12:47:14 +0530

    fix: update height calculation in ChatPage and HomePage for consistent layout

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -9,7 +9,7 @@ export const ChatPage = () => {
   return (
     <div className="h-screen bg-base-200">
       <div className="flex items-center pt-20 px-4">
-        <div className="bg-base-100 rounded-lg shadow-xl w-full max-w-6xl overflow-scroll h-[calc(100vh-2rem)]">
+        <div className="bg-base-100 rounded-lg shadow-xl w-full max-w-6xl overflow-scroll h-[calc(100vh-3rem)]">
           <div className="flex h-full rounded-lg overflow-scroll">
             <Sidebar />
             {!selectedUser ? <NoChatSelected /> : <ChatContainer />}
