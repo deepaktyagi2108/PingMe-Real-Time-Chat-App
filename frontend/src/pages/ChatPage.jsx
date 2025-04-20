d629ce338ba3a310a22b5cca4010e8e8983069b3
DEEPAK.TYAGI <tyagideepak607@gmail.com>
2025-04-20 12:36:31 +0530

    fix: adjust ChatPage container height and overflow behavior for better layout

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -9,8 +9,8 @@ export const ChatPage = () => {
   return (
     <div className="h-screen bg-base-200">
       <div className="flex items-center pt-20 px-4">
-        <div className="bg-base-100 rounded-lg shadow-xl w-full max-w-6xl h-[calc(100vh-8rem)]">
-          <div className="flex h-full rounded-lg overflow-hidden">
+        <div className="bg-base-100 rounded-lg shadow-xl w-full max-w-6xl overflow-scroll h-[calc(100vh-2rem)]">
+          <div className="flex h-full rounded-lg overflow-scroll">
             <Sidebar />
             {!selectedUser ? <NoChatSelected /> : <ChatContainer />}
           </div>
